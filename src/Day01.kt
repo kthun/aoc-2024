1696b40118e6694f1b4e44f7175e927a671ca3cb
Kristian Thun <kristian.thun@sintef.no>
2025-01-08 14:18:13 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,34 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (listA, listB) = parseInput(input)
+
+        return listA.sorted().zip(listB.sorted()).sumOf { (a, b) ->
+            abs(a - b)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (listA, listB) = parseInput(input)
+
+        return listA.sumOf { a ->
+            a * listB.count { it == a }
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
+
+    check(part1(testInput) == 11)
     part1(input).println()
+
+    check(part2(testInput) == 31)
     part2(input).println()
 }
+
+fun parseInput(input: List<String>) = input.map { line ->
+    val first = line.substringBefore(" ").toInt()
+    val second = line.substringAfterLast(" ").toInt()
+    first to second
+}.unzip()
